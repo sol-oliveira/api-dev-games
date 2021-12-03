696a8811f7c62acc82afeb7e3fb0c8930bdbda10
Sol <sol.oliveira@elevenfinancial.com>
2021-12-03 14:12:54 -0300

    creation Entities

---
+++ DevGames.API/Entities/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevGames.API.Entities
+{
+    public class Comment
+    {
+        public Comment(string title, string description, string user)
+        {
+            Title = title;
+            Description = description;
+            User = user;
+
+            CreateAt = DateTime.Now;
+        }
+
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string User { get; private set; }
+        public DateTime CreateAt { get; private set; }
+
+    }
+}

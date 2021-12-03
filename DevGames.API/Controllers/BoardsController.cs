35d579e38168acf97cccccb4d3ec058e9acf626a
Sol <sol.oliveira@elevenfinancial.com>
2021-12-03 08:49:20 -0300

    creation controllers Boards

---
+++ DevGames.API/Controllers/BoardsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevGames.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {  
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+    }
+}
